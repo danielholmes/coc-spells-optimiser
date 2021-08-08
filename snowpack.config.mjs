f3d160a395e2dd5a3b3e2b823e4cdb577a506e1e
Daniel Holmes <daniel@danielholmes.org>
2021-08-08 10:22:40 +1000

    USe babel

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -6,13 +6,8 @@ export default {
   },
   plugins: [
     '@snowpack/plugin-react-refresh',
-    [
-      '@snowpack/plugin-typescript',
-      {
-        /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
-        ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
-      },
-    ],
+    '@snowpack/plugin-babel',
+    '@snowpack/plugin-typescript'
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
